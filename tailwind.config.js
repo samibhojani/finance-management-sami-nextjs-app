98268a016d4319f769ad70f2b6b9df83f19fa111
Sami Bhojani <65404958+samibhojani@users.noreply.github.com>
2024-01-16 15:25:30 +0500

    Project Setup + Created NavBar

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: [
+    "./app/**/*.{js,tx,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
